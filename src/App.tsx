5dc736cfbf0121064afb3a389135f3785a48f8ac
lekipising <leeplanke@gmail.com>
2022-04-30 15:33:48 +0200

    let there be code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import UsersTable from './components/UserTable';
+import './index.css';
+
+function App() {
+  return (
+    <UsersTable />
+  );
+}
+
+export default App;
